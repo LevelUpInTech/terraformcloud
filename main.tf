891ec63eedbeaaa3b9b5136928f2af3f6d2b004a
LevelUpInTech <72570971+LevelUpInTech@users.noreply.github.com>
2021-05-16 17:40:10 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -44,7 +44,6 @@ module "ec2_cluster" {
   instance_type          = "t2.micro"
   key_name               = "user1"
   monitoring             = true
-  vpc_security_group_ids = ["sg-12345678"]
   subnet_ids              = module.vpc.public_subnets
 
   tags = {
